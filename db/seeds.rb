b99912976c647e5a7b0a3ed2f253b6bd63835231
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-08-19 14:40:53 +0300

    add seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Clear existing data"
+HeroPower.destroy_all
+Power.destroy_all
+Hero.destroy_all
+
+puts "Create Heroes"
+heroes = Hero.create([
+    { name: 'Robert Downey Jr', super_name: 'Iron Man' },
+    { name: 'Gwen Stacy', super_name: 'Spider-Gwen' },
+    { name: 'Doreen Green', super_name: 'Squirrel Girl' }
+])
+
+puts "Creating Powers"
+powers = Power.create([
+  { name: 'Super Strength', description: 'Gives the wielder super-human strengths' },
+  { name: 'Flight', description: 'Gives the wielder the ability to fly through the skies at supersonic speed' },
+  { name: 'Telepathy', description: 'Allows the wielder to read minds and communicate mentally' }
+])
+
+puts "Creating HeroPowers associations"
+HeroPower.create([
+  { hero: heroes[0], power: powers[0], strength: 'Strong' },
+  { hero: heroes[0], power: powers[1], strength: 'Average' },
+  { hero: heroes[1], power: powers[1], strength: 'Weak' },
+  { hero: heroes[2], power: powers[2], strength: 'Average' },
+  { hero: heroes[2], power: powers[0], strength: 'Average' }
+])
+
+puts "Seed data created!"
