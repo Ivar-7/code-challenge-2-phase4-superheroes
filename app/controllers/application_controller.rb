b6b2950238fde0aef78f6d0b01b7f778475edcdf
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-08-20 12:22:07 +0300

    added not  found for all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+    private
+
+    def not_found_response exception
+        render json: { error: "#{exception.model} not found" }, status: :not_found
+    end
 end
