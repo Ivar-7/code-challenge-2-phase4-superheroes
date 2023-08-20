0ebba6ad2f060e5259ca44d3029e7954d6145e42
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-08-20 12:23:07 +0300

    done with required crud heros

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,12 @@
 class HeroesController < ApplicationController
+
+    def index
+        heroes = Hero.all
+        render json: heroes, except: [:created_at, :updated_at]
+    end
+
+    def show
+        hero = Hero.find(params[:id])
+        render json: hero, except: [:created_at, :updated_at], include: { powers: { except: [:created_at, :updated_at] }}
+    end
 end
